9f97cdbfcf4e58d49fdb273a336feb82cc395d8e
Nathan Typanski <ntypanski@gmail.com>
2014-06-03 00:37:44 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Yorgey's Course
+===============
+
+This is [Yorgey's Haskell course](http://www.seas.upenn.edu/~cis194/lectures.html) from the University of Pennsylvania.
+
+I'm going to be doing the homework assignments to get better at Haskell.
---
+++ src/hw1.hs
@@ -0,0 +1,28 @@
+{-# OPTIONS_GHC -Wall #-}
+
+import Data.Char (digitToInt)
+import Data.List (mapAccumR)
+
+-- Exercise 1: read the digits of a number
+
+toDigits :: Integer -> [Integer]
+toDigits xs = map (toInteger . digitToInt) (show xs)
+
+toDigitsRev :: Integer -> [Integer]
+toDigitsRev xs = reverse $ map (toInteger . digitToInt) (show xs)
+
+doubleEveryOther :: [Integer] -> [Integer]
+doubleEveryOther xs =
+  let doubleOdd acc x = if odd acc then x * 2 else x
+      (_, ys) = mapAccumR (\acc x -> ((acc :: Integer) + 1, doubleOdd acc x)) 0 xs
+  in ys
+
+sumDigits :: [Integer] -> Integer
+sumDigits = sum . (concat . map toDigits)
+
+validate :: Integer -> Bool
+validate n = let digits    = toDigits n
+                 doubled   = doubleEveryOther digits
+                 summed    = sumDigits doubled
+                 remainder = summed `mod` 10
+             in remainder == 0
